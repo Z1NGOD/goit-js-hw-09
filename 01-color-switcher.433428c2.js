0f1980fa7b42c6aa7500b40a780a493c9a82db59
Z1NGOD <47328250+Z1NGOD@users.noreply.github.com>
2023-06-15 17:38:56 +0000

    Deploying to gh-pages from @ Z1NGOD/goit-js-hw-09@9c349c31d00cb95e116796bf7b8c50ea323820c1 🚀

---
+++ 01-color-switcher.433428c2.js
@@ -0,0 +1,2 @@
+!function(){let t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.querySelector("body"),o=null;t.addEventListener("click",()=>{o=setInterval(()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`},1e3),t.disabled=!0}),e.addEventListener("click",()=>{clearInterval(o),t.disabled=!1})}();
+//# sourceMappingURL=01-color-switcher.433428c2.js.map
